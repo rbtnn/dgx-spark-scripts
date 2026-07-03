6bd5bd47cf99b17dd3335678493ae1be74a7063b
rbtnn <naru123456789@gmail.com>
2026-07-04 01:50:53 +0900

    fix

--- deepseekv4/run-node1.sh
+++ deepseekv4/run-node1.sh
@@ -1,7 +1,7 @@
 
 export NODE_RANK=0
 export HEADLESS=
-export MASTER_ADDR=169.254.148.251
+export MASTER_ADDR=10.0.0.10
 export HF_CACHE=$HOME/.cache/huggingface
 docker compose down
 docker compose up -d
